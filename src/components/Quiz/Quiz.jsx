5eac0fb2a05f5aabd5e59731bfe9fb9752179453
SkPipul <skjabed252@gmail.com>
2022-10-15 21:14:52 +0600

    final project

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 import './Quiz.css'
 
 function Quiz({ data }) {
-    const { id, logo, name } = data
+    const { id, logo, name, total } = data
     return (
         <div className='my-5'>
             <div className="logo">
                 <img className="bg-dark" src={logo} alt="" />
-                <div className="mt-2 d-flex justify-content-center logo-item">
-                    <h3 className="mx-5">{name}</h3>
+                <div className="mt-2 my-5 d-flex justify-content-center
+                align-items-center logo-item">
+                    <h3 className="mx-4">{name}</h3>
+                    <h5 className="me-4">Total quiz: {total}</h5>
                     <Link to={`/quiz/${id}`}>
                         <button className="btn btn-primary">Start quiz</button>
                     </Link>
