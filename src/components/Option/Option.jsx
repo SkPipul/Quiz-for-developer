bf52f276329fed5652a8a8c27ec7373f6ba053df
SkPipul <skjabed252@gmail.com>
2022-10-15 19:02:50 +0600

    toastify added

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
-const Option = ({option, handleCorrect}) => {
+const Option = ({ option, handleCorrect }) => {
     // console.log(option);
     return (
         <div>
-            <input onClick={() => handleCorrect(option)} type="radio" />
-            <small>{option}</small>
-        </div>
-    );
+            <div class="row gx-5 w-50 mx-auto">
+                <div class="col">
+                    <div class="p-3 border bg-light">
+                        <input className='mx-1' onClick={() => handleCorrect(option)} type="radio" id="child" name="age" value="child" />
+                        <label for="child"><small>{option}</small></label>
+                    </div>
+                </div>
+            </div>
+                {/* <div>
+                    <input className='mx-1' onClick={() => handleCorrect(option)} type="radio" id="child" name="age" value="child" />
+                    <label for="child"><small>{option}</small></label>
+                </div> */}
+            </div>
+            );
 };
 
-export default Option;
+            export default Option;
--- src/components/QuizDetails/QuizDetails.jsx
+++ src/components/QuizDetails/QuizDetails.jsx
@@ -1,30 +1,43 @@
 import React from 'react';
 import Option from '../Option/Option';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizDetails = ({quizAll, index}) => {
     const {question, options, correctAnswer} = quizAll;
     // console.log(quizAll);
     
     const handleAnswer = () => {
-        console.log(correctAnswer);
+        // console.log(correctAnswer);
+        toast.success(correctAnswer, {
+            position:"top-center"
+        })
+
     }
 
     function handleCorrect (answer) {
         // console.log(answer);
         if(correctAnswer === answer){
-            console.log("done");
+            // console.log("done");
+            toast.success("Wow!! you choose the correct answer", {
+                position:"top-center"
+            })
         }
         else{
-
+            toast.error('Ops! your answer is incorrect', {
+                position:"top-center"
+            })
         }
     }
 
     return (
-        <div>
-            <h4>Q:{index + 1} {question}</h4>
-            <button onClick={handleAnswer}>show ans</button>
+        <div className='container my-5'>
+            <h4><span className='text-primary'>Q:{index + 1}</span> {question}</h4>
+            {/* <button onClick={handleAnswer}>show ans</button> */}
+            <ToastContainer />
             {
                 options.map(option => <Option
+                    key={option.id}
                     option={option}
                     correctAnswer={correctAnswer}
                     handleCorrect={handleCorrect}
